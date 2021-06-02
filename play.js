acea26f55f50a73daf6b290ad7617d6018e269f7
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-02 19:33:34 +0000

    setup stdin

--- play.js
+++ play.js
@@ -4,5 +4,28 @@ const {connect} = require('./client') //-> requiring connect function from clien
 // establishes a connection with the game server
 
 
+const setupInput = (conn) => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+  
+    // process.stdin.on('data', (key) => {
+  
+  
+      if (key === '\u0003') {
+        process.exit();
+      } 
+  }
+  
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+
+}
 
 
+
+
+setupInput(connect())
