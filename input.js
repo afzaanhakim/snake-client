02fac7e1195d8880bbaf772c1b0446b33cacce9f
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-02 21:31:20 +0000

    linting and formatting

--- input.js
+++ input.js
@@ -1,45 +1,29 @@
 let connection;
-  const handleUserInput = function (key) {
-  
-      if (key === '\u0003') {
-        process.exit();
-      } 
-      if (key === "a" || key === "A"){
-        connection.write("Move: left")
-        console.log("left")
-      }
-      if (key === "w" || key === "W"){
-        connection.write("Move: up")
-        console.log("up")
-      }
-
-      if (key === "d" || key === "D"){
-        connection.write("Move: right")
-        console.log("right")
-      }
-
-      if (key === "s" || key === "S"){
-        connection.write("Move: down")
-        console.log("down")
-      }
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } if (key === "a" || key === "A") {
+    connection.write("Move: left");
+  } if (key === "w" || key === "W") {
+    connection.write("Move: up");
+  } if (key === "d" || key === "D") {
+    connection.write("Move: right");
+  } if (key === "s" || key === "S") {
+    connection.write("Move: down");
   }
+};
 
- const setupInput = (conn) => {
-  
+const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  
   stdin.on("data", (key) => {
-    console.log(key)
-    handleUserInput(key)
+    handleUserInput(key);
 
   });
-
   return stdin;
-
-}
-module.exports = {setupInput}
+};
+module.exports = {setupInput};
