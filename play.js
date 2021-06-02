64cf50e263c18c8ba1efd637072a3d71d624d283
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-02 20:36:40 +0000

    updated the file with require logic for setupInput

--- play.js
+++ play.js
@@ -1,31 +1,8 @@
 const net = require("net");
 const {connect} = require('./client') //-> requiring connect function from client file
+const {setupInput} = require("./input");
 
-// establishes a connection with the game server
+console.log("Connecting ...");
+connect();
 
-
-const setupInput = (conn) => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function (key) {
-  
-    // process.stdin.on('data', (key) => {
-  
-  
-      if (key === '\u0003') {
-        process.exit();
-      } 
-  }
-  
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-
-}
-
-
-
-
-setupInput(connect())
+setupInput();
