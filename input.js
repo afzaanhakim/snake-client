410a0ffab08574bfe083605815680663ec50bf47
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-02 21:25:08 +0000

    implemented functionality to move the snake by implementing keys and sending that information to the server by calling connect inside setupInput

--- input.js
+++ input.js
@@ -1,21 +1,43 @@
-const setupInput = (conn) => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+let connection;
   const handleUserInput = function (key) {
-  
-    // process.stdin.on('data', (key) => {
-  
   
       if (key === '\u0003') {
         process.exit();
       } 
-  
+      if (key === "a" || key === "A"){
+        connection.write("Move: left")
+        console.log("left")
+      }
+      if (key === "w" || key === "W"){
+        connection.write("Move: up")
+        console.log("up")
+      }
 
+      if (key === "d" || key === "D"){
+        connection.write("Move: right")
+        console.log("right")
+      }
+
+      if (key === "s" || key === "S"){
+        connection.write("Move: down")
+        console.log("down")
+      }
   }
+
+ const setupInput = (conn) => {
   
-  stdin.on("data", handleUserInput);
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  
+  stdin.on("data", (key) => {
+    console.log(key)
+    handleUserInput(key)
+
+  });
 
   return stdin;
 
