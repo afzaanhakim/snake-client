a5c99530dd52f97880cc0a5e39e4760512bcf6fc
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-02 17:22:52 +0000

    removed comments that were not needed and created module.exports to export the connect function

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+
+  
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on ("connect", () => {
+    console.log("have been added")
+    
+    });
+    
+  conn.on('data', (data) => {
+    console.log(`Message from server: ${data}`);
+    });
+
+  return conn;
+
+ 
+
+};
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect};
