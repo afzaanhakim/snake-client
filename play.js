7a6b8323ff141fe4a408a4ba9a7220575d52b909
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-02 17:22:13 +0000

    moved the connect function to client file and used ES6 syntax to require the connect function from that file

--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 const net = require("net");
+const {connect} = require('./client') //-> requiring connect function from client file
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542}, () => {
-    //console.log("connection established")
-  });
-
-  
 
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on ("connect", () => {
-    console.log("have been added")
-    
-    })
 
-  return conn;
 
- 
-
-};
-
-console.log("Connecting ...");
-connect();
